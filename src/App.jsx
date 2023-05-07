629cf8f6ad990b249a87ea62f020e3915a0047ba
MrLearn <learnmr67@gmail.com>
2023-05-08 00:43:51 +0300

    получение данных для popup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
+import { useSelector } from "react-redux";
 import { Route, Routes } from "react-router-dom";
 import { Header } from "./components/Header/Header";
 import { Home } from "./components/Home/Home";
 import { Popup } from "./components/Popup/Popup";
 
 function App() {
+  const { modal } = useSelector(state => state);
+  const { list, week, dayList, isLoading } = useSelector(({ weather }) => weather);  
+
   return (
     <div className="global-container">
-      {/* <Popup /> */}
+      {modal.modal &&
+        <Popup data = {dayList}/>
+      }
       <div className="container">
         <Header />
         <Routes>
