979f03633f5a0113c072b1cafdc1f8ae6c8f0e13
MrLearn <learnmr67@gmail.com>
2023-06-05 00:40:27 +0300

    добавил смену темы

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import changeThemeSlice from "./slices/changeThemeSlice";
 import fetchWeatherSlice from "./slices/fetchWeatherSlice";
 import modalPopupSlice from "./slices/modalPopupSlice";
 
 export const store = configureStore({
     reducer: {
         weather: fetchWeatherSlice,
-        modal: modalPopupSlice, 
+        modal: modalPopupSlice,
+        theme: changeThemeSlice,
     },
 }) 
