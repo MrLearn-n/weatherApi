01c471572b0252e16037ecf071689cedb5fc3310
MrLearn <learnmr67@gmail.com>
2023-06-05 00:43:21 +0300

    логика смены тема => useEffect, сохранение темы и названия города после перезагрузки страницы => localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { Route, Routes } from "react-router-dom";
 import { Header } from "./components/Header/Header";
@@ -5,22 +6,41 @@ import { Home } from "./components/Home/Home";
 import { Popup } from "./components/Popup/Popup";
 
 function App() {
-  const { modal } = useSelector(state => state);
-  const { list, week, dayList, isLoading } = useSelector(({ weather }) => weather);  
+    const { modal } = useSelector((state) => state);
+    const { list, week, dayList, isLoading } = useSelector(
+        ({ weather }) => weather
+    );
 
-  return (
-    <div className="global-container">
-      {modal.modal &&
-        <Popup data = {dayList}/>
-      }
-      <div className="container">
-        <Header />
-        <Routes>
-          <Route path="" element = {<Home /> } />
-        </Routes>
-      </div>
-    </div>
-  );
+    const storageTheme = localStorage.getItem('theme');
+    const root = document.querySelector(':root');
+
+    const components = [
+        "body-background",
+        "components-background",
+        "card-background",
+        "text-color",
+    ];
+
+    useEffect(() => {
+        components.forEach((item) => {
+            root.style.setProperty(
+                `--${item}-default`,
+                `var(--${item}-${storageTheme})`
+            );
+        });
+    }, [storageTheme]);
+
+    return (
+        <div className="global-container">
+            {modal.modal && <Popup data={dayList} />}
+            <div className="container">
+                <Header />
+                <Routes>
+                    <Route path="" element={<Home />} />
+                </Routes>
+            </div>
+        </div>
+    );
 }
 
 export default App;
